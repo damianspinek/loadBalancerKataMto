59b1f6ae1fb2dfc9f34a601576f1547c64e1f71f
Damian Spinek <damian00737@gmail.com>
2015-05-11 19:30:00 +0200

    test 3 refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -4,6 +4,7 @@ import org.hamcrest.Matcher;
 
 public class Server {
 
+	private static final double MAX_LOAD = 100.0d;
 	public double currentLoadPecentage;
 	public int capacity;
 
@@ -14,5 +15,9 @@ public class Server {
 	public boolean contains(Vm theVm) {
 		// TODO Auto-generated method stub
 		return true;
+	}
+	
+	public void addVm(Vm vm){
+		this.currentLoadPecentage = (double)(vm.size / (double )this.capacity) * MAX_LOAD;
 	}
 }
