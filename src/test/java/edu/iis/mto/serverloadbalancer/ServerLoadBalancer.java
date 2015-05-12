9e3fae75e717f4c476cac77d175bfbf0fa5b8f89
Damian Spinek <damian00737@gmail.com>
2015-05-12 10:15:59 +0200

    test 6 refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -8,17 +8,22 @@ public class ServerLoadBalancer {
 	public void balance(Server[] servers, Vm[] vms) {
 		for(Vm vm : vms){
 		List<Server> serversWithEnoughCapacity = new ArrayList<Server>();
-		for(Server server : servers){
-			if(server.canFit(vm)){
-				serversWithEnoughCapacity.add(server);
-			}
-		}
+		findServersWithEnoughCapacity(servers, vm, serversWithEnoughCapacity);
 		Server lessLoadedServer = extractLessLoadedServer(serversWithEnoughCapacity);
 		if(lessLoadedServer != null){
 			lessLoadedServer.addVm(vm);
 		}
 		}
 		
+	}
+
+	private void findServersWithEnoughCapacity(Server[] servers, Vm vm,
+			List<Server> serversWithEnoughCapacity) {
+		for(Server server : servers){
+			if(server.canFit(vm)){
+				serversWithEnoughCapacity.add(server);
+			}
+		}
 	}
 
 	private Server extractLessLoadedServer(List<Server> serversWithEnoughCapacity) {
